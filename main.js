f02a258e613d96fcaf63fca46d488ec2b2553dcb
anoshmisiosdev <88525350+anoshmisiosdev@users.noreply.github.com>
2023-09-22 10:27:40 -0700

    merge branches

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-const menuBtn = document.querySelector('menubutton');
-let menuOpen = false;
-menubutton.addEventListener('click', () => {
-if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;} else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
+/**
+ * The Sign-In client object.
+ */
+var auth2;
+/**
+ * Initializes the Sign-In client.
+ */
+var initClient = function() {
+    gapi.load('auth2', function(){
+        /**
+         * Retrieve the singleton for the GoogleAuth library and set up the
+         * client.
+         */
+        auth2 = gapi.auth2.init({
+            client_id: '125257763090-kad4a17muhdr70ujqfchq20e3fc30jl9.apps.googleusercontent.com'
+        });
+
+        // Attach the click handler to the sign-in button
+        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
     });
+};
+
+/**
+ * Handle successful sign-ins.
+ */
+var onSuccess = function(user) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+    window.location.replace("/dashboard/hegetsus/404.html")
+};
+if (auth2.isSignedIn.get()) {
+    var profile =
+        auth2.currentUser.get().getBasicProfile();
+    console.log('Full Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail());
+}
