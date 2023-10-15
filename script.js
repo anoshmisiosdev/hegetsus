6520e30d161d9cdb9439c00f9c2896938ce353ec
Varin764 <varinisthebest@gmail.com>
2023-10-15 15:03:26 -0700

    nice

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
-    spaceBetween: 30,
-    
-
-
-
-
-
-
-
-
-
-
-
-
-})
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
