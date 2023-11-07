b56936fb599bd275f4bab7e887f28bf6806c465b
Varin764 <varinisthebest@gmail.com>
2023-11-07 10:34:50 -0800

    ghjkil

--- contact.php
+++ contact.php
@@ -5,18 +5,32 @@
 </head>
 
 <?php
-if($_SERVER[REQUEST_METHOD]=="POST"){
-    $name = htmlspecialchars($_POST("fullname"))
-    $email = htmlspecialchars($_POST("email"))
-    $message = htmlspecialchars($_POST("message"))
-    var_dump($name)
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $ini="";
+    ini_get($ini);
+    echo $ini;
+    $ini =intval($ini);
+    echo $ini;
+    echo"<br>";
+    echo"<there shouold be ini>";
+    $name = htmlspecialchars($_POST["fullname"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+    var_dump($name);
     echo("<br>");
-    var_dump($email)
+    var_dump($email);
     echo("<br>");
-    var_dump($message)
-
+    var_dump($message);
+    $subject="CSP Website feedback from $name";
+    $headers = "From: $email";
+    $to="varinisthebest@gmail.com";
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email sending failed. Please try again later.";
+    }
 }
 else{
-    header("contact.html")
+    header("contact.html");
 }
 ?>
