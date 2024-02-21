47901c1ba463caba78e24d84ab98cfef58ad6b4c
spiknspik123 <spiknspik123@gmail.com>
2024-02-21 08:58:35 -0800

    A

--- aboutrishaan.php
+++ aboutrishaan.php
@@ -34,23 +34,7 @@
     <p><br></br></p>
 
     <?php
-            #Connection Values
-            $servername = "localhost";
-            $username = "root";
-            $password = '123456';
-            #$password = ""; # Mr Millard's password is an empty string.
-
-            // Create connection to MySQL
-            $conn = new mysqli($servername, $username, $password);
-
-            // Test connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            
-            # Draw data from database using the query() method
-            $conn->query("use teamtechsupport;"); #First select the active database
-            
+            include "connect.php";
             $newquery = 'select mNAMES,mBIO from members where mID = 5';
             
             $newresult = $conn->query($newquery);
---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+            #Connection Values
+            $servername = "millard";
+            $username = "root";
+            $password = '123456';
+            #$password = ""; # Mr Millard's password is an empty string.
+
+            // Create connection to MySQL
+            $conn = new mysqli($servername, $username, $password);
+
+            // Test connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            
+            # Draw data from database using the query() method
+            $conn->query("use teamtechsupport;"); #First select the active database
+    ?>
